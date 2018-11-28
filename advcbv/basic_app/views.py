a14a2b0d8aa3dda07202c55b9a0bf5b8782910a9
nikhilchoudhary <nc1336282@gmail.com>
2018-11-29 00:52:05 +0530

    first commit

---
+++ advcbv/basic_app/views.py
@@ -0,0 +1,36 @@
+from . import models
+from django.views.generic import View, ListView, DetailView, TemplateView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+# Create your views here.
+
+
+class IndexView(TemplateView):
+    template_name = 'basic_app/index.html'
+
+
+class SchoolListView(ListView):
+    context_object_name = 'schools'
+    model = models.School
+
+
+class SchoolDetailView(DetailView):
+    context_object_name = 'school_detail'
+    model = models.School
+    template_name = 'basic_app/school_detail.html'
+
+
+class SchoolCreateView(CreateView):
+    fields = ('name', 'principal', 'location')
+    model = models.School
+
+
+class SchoolUpdateView(UpdateView):
+    fields = ('name', 'principal')
+    model = models.School
+
+
+class SchoolDeleteView(DeleteView):
+    model = models.School
+    success_url = reverse_lazy("basic_app:list")
+
